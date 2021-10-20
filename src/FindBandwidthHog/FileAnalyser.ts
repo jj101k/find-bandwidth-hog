a371d8eabf58a503053c87d2f9ca4cb885106be7
Jim Driscoll <jim.a.driscoll@gmail.com>
2021-10-20 17:37:29 +0100

    Update the file analyser to use the factory

--- src/FindBandwidthHog/FileAnalyser.ts
+++ src/FindBandwidthHog/FileAnalyser.ts
@@ -1,3 +1,9 @@
+import * as Analyser from "./Analyser"
+import { Factory } from "./Factory"
+
+/**
+ * Objects of this type can analyse a whole file and return the results.
+ */
 export class FileAnalyser {
     /**
      *
@@ -5,9 +11,23 @@ export class FileAnalyser {
      * @returns
      */
     async analyse(lines: AsyncGenerator<string> | string[]): Promise<Map<string, number>> {
+        const factory = new Factory()
+        const analysers = factory.getAnalysers()
         for await (const line of lines) {
-
+            const compiler = factory.getLineCompiler(line)
+            const compiledLine = compiler.compile(line)
+            for(const analyser of analysers) {
+                analyser.analyseLine(compiledLine)
+            }
         }
-        return new Map()
+        const bestAnalyser = analysers.reduce(
+            (c: Analyser.Base | null, i) => (c?.significance ?? -1) < i.significance ? i : c,
+            null
+        )
+        if(bestAnalyser) {
+            return bestAnalyser.results
+        } else {
+            return new Map()
+        }
     }
 }
--- test/FindBandwidthHog/FileAnalyser.test.ts
+++ test/FindBandwidthHog/FileAnalyser.test.ts
@@ -8,4 +8,12 @@ describe("File analyser tests", () => {
         const results = await analyser.analyse([])
         assert.equal(results.size, 0, "Results are empty")
     })
+    it("Can analyse a trivial sample CLF file", async () => {
+        const analyser = new FileAnalyser()
+        const results = await analyser.analyse([
+            `localhost - - [11/Mar/2021:19:53:33 +0000] "POST / HTTP/1.1" 200 9071 CUPS-Get-PPDs -`
+        ])
+        assert.equal(results.size, 1, "One result found")
+        assert.equal(results.get("/"), 1, "One instance of /")
+    })
 })
